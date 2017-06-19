3aafe8f35581223f795162f444cc9a50c2986c50
rahm <j.rahm@f5.com>
2017-06-19 15:57:18 -0500

    Initial commit

---
+++ app/config.py
@@ -0,0 +1,3 @@
+class Configuration(object):
+    DEBUG = True
+    SECRET_KEY = 'super secret key in progress here. please divert your eyes.'
---
+++ app/views.py
@@ -0,0 +1,35 @@
+from app import app
+from flask import flash
+from flask import render_template
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+
+@app.route('/challenge/<int:challenge_id>', methods=['GET'])
+def challenge(challenge_id):
+    if challenge_id == 1:
+        return render_template('one.html')
+    elif challenge_id == 2:
+        return render_template('two.html')
+    elif challenge_id == 3:
+        return render_template('three.html')
+    elif challenge_id == 4:
+        return render_template('four.html')
+    elif challenge_id == 5:
+        return render_template('five.html')
+    else:
+        flash('Not a valid challenge')
+        return render_template('index.html')
+
+
+@app.route('/challenge/2/details', methods=['GET'])
+def challenge_two_details():
+    return render_template('two_details.html')
+
+
+@app.errorhandler(404)
+def nofile(error):
+    return render_template('404.html')
